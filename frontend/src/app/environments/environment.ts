4a82976ffd83efeb71bf65b49285144ec7989876
Peter Atito <peterclermy232@gmail.com>
2026-01-10 08:19:35 +0300

    update environment

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api',
+  apiUrl: 'https://advance-company-backend-production.up.railway.app/api',
   apiTimeout: 30000
 };
