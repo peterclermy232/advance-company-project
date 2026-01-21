070abde68758ebe6f8cea96fd31de7e003fb26ad
Peter Atito <peterclermy232@gmail.com>
2026-01-21 14:09:48 +0300

    update on the document upload

--- backend/advance_company/settings.py
+++ backend/advance_company/settings.py
@@ -223,6 +223,9 @@ REST_FRAMEWORK = {
     'UNAUTHENTICATED_TOKEN': None,
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
     ],
 }
 
